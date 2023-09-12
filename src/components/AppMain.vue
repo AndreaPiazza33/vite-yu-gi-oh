b624545eece923486ad2406be34d3141179584ef
AndreaPiazza33 <andreapiazza190201@gmail.com>
2023-09-12 17:16:59 +0200

    feat: add input and fix container main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,80 +12,12 @@ export default {
   <!--contenitore main-->
   <div class="main-container">
     <!--contenitore ricerca cards-->
-    <div></div>
+    <div>
+      <input type="text" />
+    </div>
     <!--contenitore cards-->
     <div class="cards-container">
       <!--cards-->
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
-      <div class="cards">
-        <span>img card</span>
-        <h3>nome card</h3>
-        <span>tipo card</span>
-      </div>
       <div class="cards">
         <span>img card</span>
         <h3>nome card</h3>
@@ -97,11 +29,12 @@ export default {
 
 <style>
 .main-container {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
+  width: 100%;
   background-color: hsl(33.46deg 64.46% 52.55%);
   height: 100vh;
+}
+.main-container input {
+  margin: 1rem 10rem;
 }
 .cards-container {
   display: flex;
@@ -110,6 +43,7 @@ export default {
   justify-content: space-around;
   background-color: white;
   padding-top: 2rem;
+  margin: 0 auto;
   width: 80%;
 }
 .cards {
@@ -119,6 +53,5 @@ export default {
   background-color: hsl(33.46deg 64.46% 52.55%);
   margin: 1rem 1rem;
   width: calc((100% / 5) - 2rem);
-  max-height: calc(100% / 3);
 }
 </style>
